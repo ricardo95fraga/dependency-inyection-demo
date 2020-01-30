2c23c391dbe5a1d6520a08196b632549cd0cbda8
ricardo.fraga <ricardo95fraga@gmail.com>
2020-01-30 14:14:48 +0100

    Initial commit

---
+++ src/main/java/com/ricardo/dependencyinyectiondemo/DependencyInyectionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.dependencyinyectiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInyectionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyInyectionDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ricardo/dependencyinyectiondemo/DependencyInyectionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.dependencyinyectiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInyectionDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
